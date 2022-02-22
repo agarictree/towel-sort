06207cddae3f4b88242b63318cc6aabab019fe3b
agarictree <ormhild@gmail.com>
2022-02-22 16:01:58 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix){
+    return matrix.reduce((arr, current, sum) => {
+      current.sort((a, b) => !(sum & 1) ? a - b : b - a).forEach(item => arr.push(item));
+      return arr;
+    }, []);
+  } else {
+    return [];
+  }
+
 }
